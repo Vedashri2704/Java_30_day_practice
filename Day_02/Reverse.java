6b6b36f9be9128814b4eea200fbfd145182b09d5
Vedashri2704 <148053004+Vedashri2704@users.noreply.github.com>
2026-09-17 14:07:38 +0530

    Add files via upload

--- Day_02/Reverse.java
+++ Day_02/Reverse.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int reverse = 0;
+        int digit = 0;
+        while(n>0){
+            digit = n%10;
+            reverse = reverse*10+digit;
+            n/=10;
+        }
+        
+        System.out.println(reverse);
+    }
+}
